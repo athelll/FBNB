db5a731cc27d20f03e384cb78a0c3d7a1742fbf3
6felix <felix.okoronkwo.1@gmail.com>
2023-02-19 19:27:50 +0000

    console 1.0 complete

--- console.py
+++ console.py
@@ -1,26 +1,44 @@
 #!/usr/bin/env python3
+"""This is the console"""
+
+
 import cmd
 import os
 import models
+import re
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from colorama import Fore
 
+
 class FBNBCommand(cmd.Cmd):
+    """Console"""
 
     clas = {
             'BaseModel',
-            'User'
+            'User',
+            'State',
+            'City',
+            'Amenity',
+            'Place',
+            'Review'
             }
-    
+
     prompt = '({}{}) '.format(Fore.GREEN + "FBNB", Fore.RESET)
 
     def do_help(self, arg):
+        """I tell you what commands do"""
         if arg:
             try:
                 print(eval("self.do_{}.__doc__".format(arg)))
             except AttributeError:
-                print('{}{}: command does not exist'.format(Fore.RED + "ERROR", Fore.RESET))
+                print('({}{}) command does not exist'.format(
+                    Fore.RED + "ERROR", Fore.RESET))
                 return
         else:
             return super().do_help(arg)
@@ -69,9 +87,10 @@ class FBNBCommand(cmd.Cmd):
             if idd != "":
                 objs = models.storage.all()
                 for obj in objs.values():
-                    if idd == obj.id:
-                        print(obj)
-                        return
+                    if obj.__class__.__name__ == cls:
+                        if idd == obj.id:
+                            print(obj)
+                            return
                 print("** no instance found **")
             else:
                 print('** instance id missing **')
@@ -88,15 +107,16 @@ class FBNBCommand(cmd.Cmd):
             cls, idd = str(line).split()
         except ValueError:
             cls, idd = line, ""
-        
+
         if cls in self.clas:
             if idd != "":
                 objs = models.storage.all()
                 for key, obj in objs.items():
-                    if idd == obj.id:
-                        del objs[key]
-                        models.storage.save()
-                        return
+                    if obj.__class__.__name__ == cls:
+                        if idd == obj.id:
+                            del objs[key]
+                            models.storage.save()
+                            return
                 print('** no instance found **')
             else:
                 print('** instance id missing **')
@@ -118,13 +138,14 @@ class FBNBCommand(cmd.Cmd):
 
     @staticmethod
     def my_split(input_str, delimeter, arg_num):
+        """splits the arguments"""
         splitted = str(input_str).split(delimeter)
 
         sliced = [None] * arg_num
         for i, word in enumerate(splitted):
             if i < arg_num:
                 sliced[i] = word
-        # returns splitted list        
+
         return sliced
 
     def do_update(self, line):
@@ -137,14 +158,13 @@ class FBNBCommand(cmd.Cmd):
         cls, idd, attr, val = FBNBCommand.my_split(line, " ", 4)
         objects = models.storage.all().values()
         ids = [i.id for i in objects]
-      
 
         if cls not in self.clas:
             print("** class doesn't exist **")
         elif idd is None:
             print("** instance id missing **")
         elif idd not in ids:
-            print("** no instance found **")    
+            print("** no instance found **")
         elif attr is None:
             print("** attribute name missing **")
         elif val is None:
@@ -152,9 +172,57 @@ class FBNBCommand(cmd.Cmd):
         else:
             for obj in objects:
                 if idd == obj.id:
-                    val = str(val)[1:-1]
+                    if val[0] == "\"" and val[-1] == "\"":
+                        val.replace("\"", "")
                     setattr(obj, attr, val)
-                    models.storage.save() 
-                
+                    models.storage.save()
+
+    def do_count(self, line):
+        objs = models.storage.all()
+        if line in self.clas:
+            counter = 0
+            for obj in objs.values():
+                if obj.__class__.__name__ == line:
+                    counter += 1
+            print(counter)
+        else:
+            print("** no instance found **")
+
+    def default(self, line: str):
+
+        names = ["BaseModel", "User", "State", "City", "Amenity",
+                 "Place", "Review"]
+
+        commands = {"all": self.do_all,
+                    "count": self.do_count,
+                    "show": self.do_show,
+                    "destroy": self.do_destroy,
+                    "update": self.do_update}
+
+        args = re.match(r"^(\w+)\.(\w+)\((.*)\)", line)
+        if args:
+            args = args.groups()
+        if not args or len(args) < 2 or args[0] not in names \
+                or args[1] not in commands.keys():
+            super().default(line)
+            return
+
+        if args[1] in ["all", "count"]:
+            commands[args[1]](args[0])
+        elif args[1] in ["show", "destroy"]:
+            commands[args[1]](args[0] + ' ' + args[2])
+        elif args[1] == "update":
+            params = re.match(r"\"(.+?)\", (.+)", args[2])
+            if params.groups()[1][0] == '{':
+                dic_p = eval(params.groups()[1])
+                for k, v in dic_p.items():
+                    commands[args[1]](args[0] + " " + params.groups()[0] +
+                                      " " + k + " " + str(v))
+            else:
+                rest = params.groups()[1].split(", ")
+                commands[args[1]](args[0] + " " + params.groups()[0] + " " +
+                                  rest[0] + " " + rest[1])
+
+
 if __name__ == "__main__":
     FBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,19 +3,28 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+
 
 class FileStorage:
-
+    """FileStorage Class"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """returns all objects"""
         return self.__objects
-    
+
     def new(self, obj):
+        """creates a new object"""
         self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
-    
+
     def save(self):
+        """saves the new object"""
         new_dict = {}
 
         for key, obj in self.__objects.items():
@@ -25,6 +34,7 @@ class FileStorage:
             json.dump(new_dict, f, indent=2)
 
     def reload(self):
+        """reloads objects from json file"""
         try:
             with open(self.__file_path, "r") as f:
                 obj = json.load(f)
